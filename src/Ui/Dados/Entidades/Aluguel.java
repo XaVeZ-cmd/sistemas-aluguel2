78070456dc6deafba2fa27ece72ff6eb803cea36
IFNMG <IFNMG@192.168.161.14>
2019-09-17 22:37:26 -0300

    sistema aluguel de roupas

---
+++ src/Ui/Dados/Entidades/Aluguel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ui.Dados.Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author IFNMG
+ */
+public class Aluguel {
+    private Integer idAluguel;
+    private LocalDate dataAluguel;
+    private LocalDate dataDevolucao;
+
+    public Integer getIdAluguel() {
+        return idAluguel;
+    }
+
+    public void setIdAluguel(Integer idAluguel) {
+        this.idAluguel = idAluguel;
+    }
+
+    public LocalDate getDataAluguel() {
+        return dataAluguel;
+    }
+
+    public void setDataAluguel(LocalDate dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+    
+}
